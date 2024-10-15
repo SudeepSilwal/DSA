95f2388c03e09ccc0ca7bed020d4c9ea3aec9b6f
SudeepSilwal <officialsudeepsilwal@gmail.com>
2024-10-15 09:47:30 +0545

    Add files via upload

--- ArrayChunks.js
+++ ArrayChunks.js
@@ -1,29 +1,32 @@
- chunks = (array, chunkSize) => {
+
+//[1,2,3,4,5,6,7,8,9] ==> chunk([1,2,3,4,5,6,7,8,9], 3) ==> [[1,2,3],[4,5,6],[7,8,9]]
+
+chunks = (array, chunkSize) => {
 
     const myArray = []
 
     let index = 0
-
-    let remainder = array.length % chunkSize
     
-    if (remainder === 0) {
+    if (!Array.isArray(array)) {
+        console.log("The given value is not an array.");
+        return;
+      }
 
-        while (index < array.length ){
-            const chunkArray = array.slice(index, index + chunkSize)
+    while (index < array.length ){
+        const chunkArray = array.slice(index, index + chunkSize)
 
-            // console.log(chunkArray);
+        // console.log(chunkArray);
 
-            myArray.push(chunkArray);
+        myArray.push(chunkArray);
 
-            index += chunkSize;
-            
-        }
+        index += chunkSize;
         
-        return myArray
     }
+    
+    return myArray
 
  }
 
- num = [0,1,2,3,4,5,6,7,8,9]
+ num = 1345
 
-console.log(chunks(num, 2));
+console.log(chunks(num, 8));
